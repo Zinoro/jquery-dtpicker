6ecdd2123a002c9ac841a5377517d3e711ca37fd
jack <jack@ten.lan>
2009-08-27 17:21:41 +0100

    implemented timezone selection. also various changes for neatness

--- jquery-dtpicker.js
+++ jquery-dtpicker.js
@@ -37,45 +37,42 @@
         function display() {
           var l_input, l_maindiv, a, i;
           // helper classes
-          function Yeargroup(p_min_year, p_max_year) { 
+          function Timezonehour(p_hour) { 
             if (!(this instanceof arguments.callee)) { throw "Constructor called as a function"; } 
+            this.hour = p_hour; 
+            this.output = "";
+          }
+          Timezonehour.prototype.toString = function() { return "&plusmn;"+("0"+this.hour).slice(-2); };
+          function Timezoneminute(p_hour, p_minute, p_sign) { 
+            if (!(this instanceof arguments.callee)) { throw "Constructor called as a function"; } 
+            this.hour = p_hour; 
+            this.minute = p_minute; 
+            this.sign = p_sign;
+            this.part = ((p_hour.hour===0)&&(p_minute===0))?"Z":p_sign+("0"+p_hour.hour).slice(-2)+":"+("0"+p_minute).slice(-2);
+            this.output = "";
+          }
+          Timezoneminute.prototype.toString = function() { return this.sign+("0"+this.hour.hour).slice(-2)+":"+("0"+this.minute).slice(-2); };
+          function Yeargroup(p_timezone, p_min_year, p_max_year) { 
+            if (!(this instanceof arguments.callee)) { throw "Constructor called as a function"; } 
+            this.timezone = p_timezone; 
             this.min_year = p_min_year; 
             this.max_year = p_max_year; 
-            this.output_part = this.min_year+'';
-            if (l_type==="date") {
-              this.output = this.output_part+'-01-01';
-            } else if (l_type==="datetime") {
-              this.output = this.output_part+'-01-01T00:00Z';
-            } else if (l_type==="datetime-local") {
-              this.output = this.output_part+'-01-01T00:00';
-            }
+            this.output = p_min_year+"-01-01"+((l_type.slice(0, 7)==="datetime")?"T00:00":"")+((l_type==="datetime")?p_timezone.part:"");
           }
-          Yeargroup.prototype.toString = function() { return this.min_year + 's'; };
-          function Year(n) { 
+          Yeargroup.prototype.toString = function() { return this.min_year + "s"; };
+          function Year(p_timezone, n) { 
             if (!(this instanceof arguments.callee)) { throw "Constructor called as a function"; } 
+            this.timezone = p_timezone; 
             this.year = n; 
-            this.output_part = this.year+'';
-            if (l_type==="date") {
-              this.output = this.output_part+'-01-01';
-            } else if (l_type==="datetime") {
-              this.output = this.output_part+'-01-01T00:00Z';
-            } else if (l_type==="datetime-local") {
-              this.output = this.output_part+'-01-01T00:00';
-            }
+            this.output = this.year+"-01-01"+((l_type.slice(0, 7)==="datetime")?"T00:00":"")+((l_type==="datetime")?p_timezone.part:"");
           }
-          Year.prototype.toString = function() { return this.output_part; };
+          Year.prototype.toString = function() { return this.year+""; };
           function Month(y, m) { 
             if (!(this instanceof arguments.callee)) { throw "Constructor called as a function"; } 
             this.year = y; 
             this.month = m; 
-            this.output_part = this.year.output_part+'-'+('0'+this.month).slice(-2);
-            if (l_type==="date") {
-              this.output = this.output_part+'-01';
-            } else if (l_type==="datetime") {
-              this.output = this.output_part+'-01T00:00Z';
-            } else if (l_type==="datetime-local") {
-              this.output = this.output_part+'-01T00:00';
-            }
+            this.part = y.year+"-"+("0"+this.month).slice(-2);
+            this.output = this.part+"-01"+((l_type.slice(0, 7)==="datetime")?"T00:00":"")+((l_type==="datetime")?y.timezone.part:"");
           }
           Month.prototype.toString = function() { return ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][this.month - 1]; };
           function Daygroup(p_month, p_min_day, p_max_day) { 
@@ -83,52 +80,32 @@
             this.month = p_month; 
             this.min_day = p_min_day; 
             this.max_day = p_max_day; 
-            this.output_part = this.month.output_part;
+            this.part = this.month.part;
             this.output = this.month.output;
           }
-          Daygroup.prototype.toString = function() { return this.min_day + '-' + this.max_day; };
+          Daygroup.prototype.toString = function() { return this.min_day + "-" + this.max_day; };
           function Day(p_month, p_day) { 
             if (!(this instanceof arguments.callee)) { throw "Constructor called as a function"; } 
             this.month = p_month; 
             this.day = p_day; 
-            this.output_part = this.month.output_part+'-'+("0"+this.day).slice(-2);
-            if (l_type==="date") {
-              this.output = this.output_part;
-            } else if (l_type==="datetime") {
-              this.output = this.output_part+'T00:00Z';
-            } else if (l_type==="datetime-local") {
-              this.output = this.output_part+'T00:00';
-            }
+            this.part = this.month.part+"-"+("0"+this.day).slice(-2);
+            this.output = this.part+((l_type.slice(0, 7)==="datetime")?"T00:00":"")+((l_type==="datetime")?p_month.year.timezone.part:"");
           }
           Day.prototype.toString = function() { return this.day; };
           function Hour(p_day, p_hour) { 
             if (!(this instanceof arguments.callee)) { throw "Constructor called as a function"; } 
             this.day = p_day; 
             this.hour = p_hour; 
-            if (l_type==="time") {
-              this.output_part = ("0"+this.hour).slice(-2);
-            } else {
-              this.output_part = this.day.output_part+"T"+("0"+this.hour).slice(-2);
-            }
-            if (l_type==="datetime") {
-              this.output = this.output_part+':00Z';
-            } else if (l_type==="datetime-local") {
-              this.output = this.output_part+':00';
-            } else if (l_type==="time") {
-              this.output = this.output_part+':00';
-            }
+            this.part = ((l_type==="time")?"":this.day.part+"T")+("0"+this.hour).slice(-2);
+            this.output = this.part+":00"+((l_type==="datetime")?p_day.month.year.timezone.part:"");
           }
           Hour.prototype.toString = function() { return ("0"+this.hour).slice(-2)+":"; };
           function Minute(p_hour, p_minute) { 
             if (!(this instanceof arguments.callee)) { throw "Constructor called as a function"; } 
             this.hour = p_hour; 
             this.minute = p_minute; 
-            this.output_part = this.hour.output_part+":"+("0"+this.minute).slice(-2);
-            if (l_type==="datetime") {
-              this.output = this.output_part+'Z';
-            } else {
-              this.output = this.output_part;
-            }
+            this.part = this.hour.part+":"+("0"+this.minute).slice(-2);
+            this.output = this.part+((l_type==="datetime")?p_hour.day.month.year.timezone.part:"");
           }
           Minute.prototype.toString = function() { return ("0"+this.minute).slice(-2); };
           // element populator
@@ -158,6 +135,8 @@
                     break;
                   }
                 }
+              } else if (a[0] instanceof Timezonehour) {
+                o = 0;
               }
             }
             m = 12;
@@ -165,16 +144,16 @@
             if(o+m<a.length) { m -= 1; }
             if(this===l_maindiv) {
               $(this).children().remove();
-              d = $(this).append("<div></div>").children("div").css({'float':'left', 'background':'#F2F2F2', 'padding':'0px', 'margin':'1px'});
+              d = $(this).append("<div></div>").children("div").css({"float":"left", "background":"#F2F2F2", "padding":"0px", "margin":"1px"});
             } else {
               $(this).parent().children("div").css("border", "1px solid #E2E2E2");
               $(this).css("border", "1px solid black");
               $(this).parent().nextAll("div").remove();
-              d = $(this).parent().after("<div></div>").next("div").css({'float':'left', 'background':'#F2F2F2', 'padding':'0px', 'margin':'1px'});
+              d = $(this).parent().after("<div></div>").next("div").css({"float":"left", "background":"#F2F2F2", "padding":"0px", "margin":"1px"});
             }
             if (o>0) {
               d.append("<div><label>&uarr;</label></div>").children("div:last-child")
-                  .css({'border':'1px solid #E2E2E2', 'background':'#E2E2E2', 'padding':'3px', 'margin':'2px', 'text-align':'center'})
+                  .css({"border":"1px solid #E2E2E2", "background":"#E2E2E2", "padding":"3px", "margin":"2px", "text-align":"center"})
                   .click(function(event){
                     event.stopPropagation();
                     l_input.focus();
@@ -184,7 +163,7 @@
             for (i=o; i<o+Math.min(a.length, m); i++){ 
               (function(e){
                 d.append("<div><label>" + e + "</label></div>").children("div:last-child")
-                    .css({'border':'1px solid #E2E2E2', 'background':'#E2E2E2', 'padding':'3px', 'margin':'2px'})
+                    .css({"border":"1px solid #E2E2E2", "background":"#E2E2E2", "padding":"3px", "margin":"2px"})
                     .click(function(event){ 
                       event.stopPropagation();
                       l_input.val(e.output);
@@ -199,7 +178,7 @@
             }
             if (a.length>o+m) {
               d.append("<div><label>&darr;</label></div>").children("div:last-child")
-                  .css({'border':'1px solid #E2E2E2', 'background':'#E2E2E2', 'padding':'3px', 'margin':'2px', 'text-align':'center'})
+                  .css({"border":"1px solid #E2E2E2", "background":"#E2E2E2", "padding":"3px", "margin":"2px", "text-align":"center"})
                   .click(function(event){ 
                     event.stopPropagation();
                     l_input.focus();
@@ -218,7 +197,7 @@
             l_input.one("focus", display);
           }	
           l_maindiv = l_input.after("<div></div>").next("div")
-              .css({'display':'inline', 'position':'absolute', 'float':'left', 'z-index':'1', 'background':'white', 'padding':'2px'})
+              .css({"display":"inline", "position":"absolute", "float":"left", "z-index":"1", "background":"white", "padding":"2px"})
               .blur(function(){ 
                 die();
               })
@@ -247,9 +226,7 @@
               l_this_max = (((p.day===undefined)||((p.day.month.year.year===l_max_year)&&(p.day.month.month===l_max_month)&&(p.day.day===l_max_day)))&&(p.hour===l_max_hour))?l_max_minute:55;
               a = [];
               for (i=l_this_min; i<=l_this_max; i+=5) { a.push(new Minute(p, i)); }
-              do_elements.call(s, a, function(s, p){
-                do_elements.call(s, [], function(){});
-              });
+              do_elements.call(s, a, function(s, p){ do_elements.call(s, [], function(){}); });
             });
           }
           function do_from_day(s, p) {
@@ -265,7 +242,7 @@
           function do_from_year(s, p) {
             var a, i;
             a = [];
-            for (i=Math.max(l_min_year, p.min_year); i<=Math.min(l_max_year, p.max_year); i++) { a.push(new Year(i)); }
+            for (i=Math.max(l_min_year, p.min_year); i<=Math.min(l_max_year, p.max_year); i++) { a.push(new Year(p.timezone, i)); }
             do_elements.call(s, a, function(s, p){
               var a, i;
               a = [];
@@ -289,19 +266,38 @@
               });
             });
           }
-          function do_from_yeargroup() {
+          function do_from_yeargroup(s, p) {
+            var a, i;
             a = [];
-            for (i=l_min_decade; i<=l_max_decade; i++) { a.push(new Yeargroup(i*10, i*10+9)); }
-            do_elements.call(l_maindiv, a, do_from_year);
+            for (i=l_min_decade; i<=l_max_decade; i++) { a.push(new Yeargroup(p, i*10, i*10+9)); }
+            do_elements.call(s, a, do_from_year);
+          }
+          function do_from_timezone() {
+            var a, i;
+            a = [];
+            for (i=0; i<=23; i++) { a.push(new Timezonehour(i)); }
+            do_elements.call(l_maindiv, a, function(s, p){
+              var a, i;
+              a = [];
+              if(p.hour===0) {
+                a.push(new Timezoneminute(p, 0, ""));
+                for (i=15; i<=45; i+=15) { a.push(new Timezoneminute(p, i, "-")); a.push(new Timezoneminute(p, i, "+")); } 
+              } else {
+                for (i=0; i<=45; i+=15) { a.push(new Timezoneminute(p, i, "-")); a.push(new Timezoneminute(p, i, "+")); } 
+              }
+              do_elements.call(s, a, do_from_yeargroup);
+            });
           }
           // entry point
           if (l_type==="time") {
             do_from_time(l_maindiv);
+          } else if(l_type==="datetime") {
+            do_from_timezone();
           } else {
             if (l_min_year>=l_this_year-1) {
-              do_from_year(l_maindiv, new Yeargroup(l_min_year, l_max_year));
+              do_from_year(l_maindiv, new Yeargroup(undefined, l_min_year, l_max_year));
             } else {
-              do_from_yeargroup();
+              do_from_yeargroup(l_maindiv);
             }
           }
         }
